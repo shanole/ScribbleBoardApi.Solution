0399c02cb3b00e490bd497330450ff356e619fc6
shanole <73411129+shanole@users.noreply.github.com>
2021-07-12 00:23:03 -0700

    add new properties for user info

--- ScribbleBoardApi/Models/Image.cs
+++ ScribbleBoardApi/Models/Image.cs
@@ -7,10 +7,10 @@ namespace ScribbleBoardApi.Models
     public int ImageId {get; set;}
     // this needs to be converted between base64 string with Convert.FromBase64String() or Convert.ToBase64String();
     public string Data {get; set;}
-    // this should be a string
-    // public int UserId {get; set;}
     public string Title {get; set;}
     public string Description {get; set;}
     public DateTime CreatedAt {get; set;}
-  }
+    public string UserId {get; set;}
+    public string UserName {get; set;}
+  }    
 }
